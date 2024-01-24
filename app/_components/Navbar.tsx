33d9a386947a0e0767864e7e521ea577fdf38e7f
Jason <jasonkwm13@gmail.com>
2024-01-24 23:48:44 +0800

    feat: make it similar to original, color scheme for dropdown nav and logo

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import DonateButton from "./DonateButton";
 const Navbar = () => {
-	const LinkStyles = "p-4 hover:scaleHover scaleHoverT";
-	const activeStyle = "bg-transparent lg:bg-black/10 font-medium";
-	const [toggle, setToggle] = useState(false);
-	const handleClick = () => setToggle((prevToggle: boolean) => !prevToggle);
-
+	const LinkStyles = "p-4 hover:scaleHover scaleHoverT font-medium text-sm";
+	const activeStyle = "bg-transparent lg:bg-black/10 font-semibold";
 	const sectionIds = ["introduction", "mission", "what-we-do", "donation", "testimonial", "footer"];
+
+	const [toggle, setToggle] = useState(false);
 	const [activeSection, setActiveSection] = useState(sectionIds[0]);
+
+	const handleClick = () => setToggle((prevToggle: boolean) => !prevToggle);
 
 	useEffect(() => {
 		const observer = new IntersectionObserver(
@@ -40,44 +41,63 @@ const Navbar = () => {
 		};
 		// eslint-disable-next-line
 	}, [activeSection]);
-	const navStyle = `${LinkStyles} ${activeSection === "" ? activeStyle : ""}`;
 	return (
 		<>
-			<header className="flex flex-row justify-between items-center py-4 gap-x-2.5 w-full md:max-w-screen-xl mx-auto px-8 md:px-16">
-				<a href="#introduction">
-					<Image
-						src="/logo.png"
-						width={64}
-						height={64}
-						alt="a picture of me, jason."
-						className="w-10 md:w-16"
-					/>
-				</a>
-				<div>
-					<nav className="hidden md:flex md:justify-around items-center">
-						<a className={navStyle} href="#introduction">
-							Introduction
-						</a>
-						<a className={navStyle} href="#mission">
-							Mission
-						</a>
-						<a className={navStyle} href="#what-we-do">
-							What We Do
-						</a>
-						<a className={navStyle} href="#testimonials">
-							Testimonials
-						</a>
-						<a className={navStyle} href="#contact">
-							Contact
-						</a>
-					</nav>
-					<BurgerButton {...{ toggle, handleClick }} />
+			<header
+				className={`fixed top-0 left-0 w-full
+    z-10 ${toggle ? "bg-black" : "bg-cream"} shadow-md`}
+			>
+				<div className="flex flex-row justify-between items-center py-4 gap-x-2.5 w-full md:max-w-screen-xl mx-auto px-8 md:px-16">
+					<a href="#introduction">
+						<Image
+							src="/logo.png"
+							width={140}
+							height={80}
+							alt="red logo with some text at bottom"
+							className="w-10 md:w-16"
+						/>
+					</a>
+					<div>
+						<nav className="hidden md:flex md:justify-around items-center">
+							<a
+								className={`${LinkStyles} ${activeSection === "introduction" ? activeStyle : ""}`}
+								href="#introduction"
+							>
+								Introduction
+							</a>
+							<a
+								className={`${LinkStyles} ${activeSection === "mission" ? activeStyle : ""}`}
+								href="#mission"
+							>
+								Mission
+							</a>
+							<a
+								className={`${LinkStyles} ${activeSection === "what-we-do" ? activeStyle : ""}`}
+								href="#what-we-do"
+							>
+								What We Do
+							</a>
+							<a
+								className={`${LinkStyles} ${activeSection === "testimonials" ? activeStyle : ""}`}
+								href="#testimonials"
+							>
+								Testimonials
+							</a>
+							<a
+								className={`${LinkStyles} ${activeSection === "contact" ? activeStyle : ""}`}
+								href="#contact"
+							>
+								Contact
+							</a>
+						</nav>
+						<BurgerButton {...{ toggle, handleClick }} />
+					</div>
+					<div className="hidden lg:block">
+						<DonateButton />
+					</div>
 				</div>
-				<div className="hidden lg:block">
-					<DonateButton />
-				</div>
+				<MobileNavList {...{ toggle, handleClick }} />
 			</header>
-			<MobileNavList {...{ toggle, handleClick }} />
 		</>
 	);
 };
@@ -91,17 +111,21 @@ const BurgerButton = ({ toggle, handleClick }: burgerButtonProps) => {
 	return (
 		<div onClick={handleClick} className={`md:hidden transition-all duration-300`}>
 			<div
-				className={`w-8 h-[2px] rounded-full bg-black mb-2  transition-all duration-300 ${
+				className={`${
+					toggle ? "bg-white" : "bg-black"
+				} w-8 h-[2px] rounded-full mb-2  transition-all duration-300 ${
 					toggle ? "translate-y-[10px] -rotate-45 " : ""
 				}`}
 			></div>
 			<div
-				className={`w-8 h-[2px] rounded-full bg-black mb-2  transition-all duration-300 ${
-					toggle ? "opacity-0 " : ""
-				}`}
+				className={`${
+					toggle ? "bg-white" : "bg-black"
+				} w-8 h-[2px] rounded-full mb-2  transition-all duration-300 ${toggle ? "opacity-0 " : ""}`}
 			></div>
 			<div
-				className={`w-8 h-[2px] rounded-full bg-black  transition-all duration-300 ${
+				className={`${
+					toggle ? "bg-white" : "bg-black"
+				} w-8 h-[2px] rounded-full  transition-all duration-300 ${
 					toggle ? "translate-y-[-10px] rotate-45 " : ""
 				}`}
 			></div>
@@ -110,26 +134,27 @@ const BurgerButton = ({ toggle, handleClick }: burgerButtonProps) => {
 };
 
 const MobileNavList = ({ toggle, handleClick }: burgerButtonProps) => {
-	const LinkStyles = "py-6 px-8 text-right border-b-[1px] border-gray font-semibold text-sm";
+	const linkStyle =
+		"py-6 px-8 text-right border-b-[1px] border-gray font-semibold text-sm text-white border-mineShaft";
 	return (
 		<nav
-			className={`bg-white w-full mobile-nav-div fixed md:hidden z-10 flex flex-col justify-end ${
+			className={`bg-black w-full fixed md:hidden z-10 flex flex-col justify-end ${
 				toggle ? "block" : "hidden"
 			}`}
 		>
-			<a className={`${LinkStyles} border-t-[1px]`} onClick={handleClick} href="#introduction">
+			<a className={`${linkStyle} border-t-[1px]`} onClick={handleClick} href="#introduction">
 				Introduction
 			</a>
-			<a className={`${LinkStyles}`} onClick={handleClick} href="#mission">
+			<a className={`${linkStyle}`} onClick={handleClick} href="#mission">
 				Mission
 			</a>
-			<a className={`${LinkStyles}`} onClick={handleClick} href="#what-we-do">
+			<a className={`${linkStyle}`} onClick={handleClick} href="#what-we-do">
 				What We Do
 			</a>
-			<a className={`${LinkStyles}`} onClick={handleClick} href="#testimonials">
+			<a className={`${linkStyle}`} onClick={handleClick} href="#testimonials">
 				Testimonials
 			</a>
-			<a className={`${LinkStyles} shadow-xl`} onClick={handleClick} href="#contact">
+			<a className={`${linkStyle} shadow-xl`} onClick={handleClick} href="#contact">
 				Contact
 			</a>
 		</nav>
