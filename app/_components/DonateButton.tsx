5b19951c9011a1ee65fd8c9bfa99d67c54046f7b
Jason <jasonkwm13@gmail.com>
2024-01-24 23:13:53 +0800

    feat: donate button

---
+++ app/_components/DonateButton.tsx
@@ -0,0 +1,22 @@
+type DonateButtonProps = {
+	text?: string;
+};
+
+const DonateButton = ({ text = "Donate Now" }) => {
+	return (
+		<a
+			href="https://docs.google.com/forms/d/e/1FAIpQLSfNdxpuKbsNqgBAx_to1Sgkl4q_n4wdlErPjVWPHsC-w4n_qQ/viewform?usp=sf_link"
+			target="_blank"
+			className="bg-greenHaze rounded-[5px]
+            px-5 py-2.5 text-white font-medium text-button
+            hover:bg-[#039154]
+            transition-all ease-in-out duration-300
+            motion-reduce:transition-none"
+			rel="noreferrer"
+		>
+			{text}
+		</a>
+	);
+};
+
+export default DonateButton;
