40cea7a02042d0d7430a051c8a953f48a8c0dd88
Jason <jasonkwm13@gmail.com>
2024-01-26 23:42:33 +0800

    feat: done introduction section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
-
+import Introduction from "./_components/Introduction";
 export default function Home() {
-    return <main className="">some txt</main>;
+	return (
+		<main className="">
+			<Introduction />
+		</main>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,149 +46,148 @@ const config: Config = {
 					DEFAULT: "#048D52",
 				},
 			},
-			fontSize: {
-				button: [
-					"0.875rem",
-					{
-						lineHeight: "1.25rem",
-						fontWeight: "500",
-					},
-				],
-				button2: [
-					"1rem",
-					{
-						lineHeight: "1.5",
-						fontWeight: "700",
-					},
-				],
-				light: [
-					"0.875rem",
-					{
-						lineHeight: "1.25rem",
-						fontWeight: "300",
-					},
-				],
-				header: [
-					"4.5rem",
-					{
-						lineHeight: "1",
-						fontWeight: "800",
-					},
-				],
-				header3: [
-					"1.875rem",
-					{
-						lineHeight: "2.25rem",
-						fontWeight: "700",
-					},
-				],
-				header2: [
-					"2.25rem",
-					{
-						lineHeight: "2.5rem",
-						fontWeight: "700",
-					},
-				],
-				header5: [
-					"1.125rem",
-					{
-						lineHeight: "1.75rem",
-						fontWeight: "700",
-					},
-				],
-				body: [
-					"1rem",
-					{
-						lineHeight: "1.5rem",
-						fontWeight: "400",
-					},
-				],
-				body2: [
-					"0.875rem",
-					{
-						lineHeight: "1.25rem",
-						fontWeight: "400",
-					},
-				],
-				caption: [
-					"0.75rem",
-					{
-						lineHeight: "0.875rem",
-						fontWeight: "500",
-					},
-				],
-				caption2: [
-					"0.75rem",
-					{
-						lineHeight: "0.875rem",
-						fontWeight: "400",
-					},
-				],
-				caption3: [
-					"0.75rem",
-					{
-						lineHeight: "0.875rem",
-						fontWeight: "400",
-					},
-				],
-				caption4: [
-					"0.625em",
-					{
-						lineHeight: "1rem",
-						fontWeight: "400",
-					},
-				],
-				caption5: [
-					"0.5rem",
-					{
-						lineHeight: "0.75rem",
-						fontWeight: "400",
-					},
-				],
-				title1: [
-					"4.5rem",
-					{
-						lineHeight: "1",
-
-						fontWeight: "800",
-					},
-				],
-				title2: [
-					"3.75rem",
-					{
-						lineHeight: "6rem",
-						fontWeight: "700",
-					},
-				],
-				title3: [
-					"3rem",
-					{
-						lineHeight: "3rem",
-						fontWeight: "700",
-					},
-				],
-				title4: [
-					"2.75rem",
-					{
-						lineHeight: "4rem",
-						fontWeight: "800",
-					},
-				],
-				title5: [
-					"1.5rem",
-					{
-						lineHeight: "2.25rem",
-						fontWeight: "700",
-					},
-				],
-				title6: [
-					"2.5rem",
-					{
-						lineHeight: "3.75rem",
-						fontWeight: "800",
-					},
-				],
-			},
+		},
+		fontSize: {
+			button: [
+				"0.875rem",
+				{
+					lineHeight: "1.25rem",
+					fontWeight: "500",
+				},
+			],
+			button2: [
+				"1rem",
+				{
+					lineHeight: "1.5",
+					fontWeight: "700",
+				},
+			],
+			light: [
+				"0.875rem",
+				{
+					lineHeight: "1.25rem",
+					fontWeight: "300",
+				},
+			],
+			header: [
+				"4.5rem",
+				{
+					lineHeight: "1",
+					fontWeight: "800",
+				},
+			],
+			header3: [
+				"1.875rem",
+				{
+					lineHeight: "2.25rem",
+					fontWeight: "700",
+				},
+			],
+			header2: [
+				"2.25rem",
+				{
+					lineHeight: "2.5rem",
+					fontWeight: "700",
+				},
+			],
+			header5: [
+				"1.125rem",
+				{
+					lineHeight: "1.75rem",
+					fontWeight: "700",
+				},
+			],
+			body: [
+				"1rem",
+				{
+					lineHeight: "1.5rem",
+					fontWeight: "400",
+				},
+			],
+			body2: [
+				"0.875rem",
+				{
+					lineHeight: "1.25rem",
+					fontWeight: "400",
+				},
+			],
+			caption: [
+				"0.75rem",
+				{
+					lineHeight: "0.875rem",
+					fontWeight: "500",
+				},
+			],
+			caption2: [
+				"0.75rem",
+				{
+					lineHeight: "0.875rem",
+					fontWeight: "400",
+				},
+			],
+			caption3: [
+				"0.75rem",
+				{
+					lineHeight: "0.875rem",
+					fontWeight: "400",
+				},
+			],
+			caption4: [
+				"0.625em",
+				{
+					lineHeight: "1rem",
+					fontWeight: "400",
+				},
+			],
+			caption5: [
+				"0.5rem",
+				{
+					lineHeight: "0.75rem",
+					fontWeight: "400",
+				},
+			],
+			title1: [
+				"4.5rem",
+				{
+					lineHeight: "1.4",
+					fontWeight: "700",
+				},
+			],
+			title2: [
+				"3.75rem",
+				{
+					lineHeight: "6rem",
+					fontWeight: "700",
+				},
+			],
+			title3: [
+				"3rem",
+				{
+					lineHeight: "3rem",
+					fontWeight: "700",
+				},
+			],
+			title4: [
+				"2.75rem",
+				{
+					lineHeight: "4rem",
+					fontWeight: "800",
+				},
+			],
+			title5: [
+				"1.5rem",
+				{
+					lineHeight: "2.25rem",
+					fontWeight: "700",
+				},
+			],
+			title6: [
+				"2.5rem",
+				{
+					lineHeight: "3.75rem",
+					fontWeight: "800",
+				},
+			],
 		},
 	},
 	plugins: [],
