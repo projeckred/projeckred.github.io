830af0a215da6c39e11c080fab92c55d417447d5
Jason <jasonkwm13@gmail.com>
2024-02-24 19:23:40 +0800

    chore: add lists for our teams bottom section

--- app/_modules/OurTeam.tsx
+++ app/_modules/OurTeam.tsx
@@ -1,5 +1,107 @@
 import Image from "next/image";
 
+const partnersList: LogoGalleryProps = {
+	title: "Partners",
+	logos: [
+		{
+			id: 0xaa,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xbb,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+	],
+};
+
+const appearanceList: LogoGalleryProps = {
+	title: "We appeared on:",
+	logos: [
+		{
+			id: 0xaa,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xbb,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xcc,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+	],
+};
+
+const collaborationList: LogoGalleryProps = {
+	title: "We collaborated with:",
+	logos: [
+		{
+			id: 0xaa,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xbb,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xcc,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xdd,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xee,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xff,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+		{
+			id: 0xf1,
+			src: "/teams/vision.jpg",
+			width: 100,
+			height: 100,
+			alt: "logo of a boy",
+		},
+	],
+};
+
 const OurTeam = () => {
 	return (
 		<section id="our-team">
@@ -11,8 +113,14 @@ const OurTeam = () => {
 						<div>
 							<Image
 								src={"/teams/vision.jpg"}
-								width={150}
-								height={150}
+								width={100}
+								height={100}
+								alt="image of our partners"
+							/>
+							<Image
+								src={"/teams/vision.jpg"}
+								width={100}
+								height={100}
 								alt="image of our partners"
 							/>
 						</div>
@@ -20,6 +128,36 @@ const OurTeam = () => {
 				</section>
 			</div>
 		</section>
+	);
+};
+
+type LogoGalleryProps = {
+	title: string;
+	logos: {
+		id: number;
+		src: string;
+		width: number;
+		height: number;
+		alt: string;
+	}[];
+};
+
+const LogoGallery = ({ title, logos }: LogoGalleryProps) => {
+	return (
+		<div>
+			<h4>{title}</h4>
+			<div>
+				{logos.map((logo) => (
+					<Image
+						key={logo.id}
+						src={logo.src}
+						width={logo.width}
+						height={logo.height}
+						alt={logo.alt}
+					/>
+				))}
+			</div>
+		</div>
 	);
 };
 
