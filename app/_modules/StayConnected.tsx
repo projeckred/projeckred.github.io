89b113c64edbd704c7099e5ec529e44e01841ded
Jason <jasonkwm13@gmail.com>
2024-02-25 17:32:01 +0800

    feat: add code for stay conntected

--- app/_modules/StayConnected.tsx
+++ app/_modules/StayConnected.tsx
@@ -1,6 +1,20 @@
 "use client";
 
 const StayConnected = () => {
+	function handleSubmit(e: any): void {
+		e.preventDefault();
+		const data = new FormData(e.target as HTMLFormElement);
+		const action = e.target.action;
+		const currentTime = new Date();
+		data.append("Datetime", currentTime.toString());
+		e.target.reset();
+		fetch(action, {
+			method: "post",
+			body: data,
+		}).then(() => {
+			alert("Thank you for reaching out!");
+		});
+	}
 	const inputStyle =
 		"bg-transparent border-solid border-2 border-white outline-none rounded-2xl font-extrabold text-center text-lg \
       placeholder:text-white placeholder:font-extrabold focus:border-white placeholder:opacity-60 py-4 px-10 md:p-0";
@@ -8,18 +22,23 @@ const StayConnected = () => {
 		<section id="stay-connected" className="bg-oyen">
 			<div className="content m-auto py-16 md:p-20 text-white px-6 md:px-0">
 				<h2 className="font-bold text-4xl md:text-5xl text-center mb-16">Stay Connected</h2>
-				<form action={""} className="flex lg:flex-row justify-around flex-col gap-4">
+				<form
+					className="flex lg:flex-row justify-around flex-col gap-4"
+					onSubmit={handleSubmit}
+					action=""
+					method="post"
+				>
 					<input
 						className={inputStyle}
 						type="text"
-						name="firstName"
+						name="first-name"
 						placeholder="FIRST NAME"
 						required
 					/>
 					<input
 						className={inputStyle}
 						type="text"
-						name="lastName"
+						name="last-name"
 						placeholder="LAST NAME"
 						required
 					/>
