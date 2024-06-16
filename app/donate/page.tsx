d9c45e1e072a45976ead90b37fdcc66395a5ff0f
Jason <jasonkwm13@gmail.com>
2024-06-16 12:46:17 +0800

    init: donate page

---
+++ app/donate/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "R.ED",
+	description: "Every little bit counts!",
+	icons: "/logo.png",
+};
+
+export default function Donate() {}
